10a03495f4dd54c41fec40df219f13fbfcb4282f
devyn2748 <devyn2748@gmail.com>
2026-08-27 00:16:57 -0400

    Cleanup patch scripts

--- patch_renderer_theme.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('src/renderer.ts', 'r') as f:
-    code = f.read()
-
-code = code.replace("theme.primary", "theme.text")
-
-with open('src/renderer.ts', 'w') as f:
-    f.write(code)
